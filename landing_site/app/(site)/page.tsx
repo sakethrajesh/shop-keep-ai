423bba8a99e09ff7dd2f4ab2b32637acfda4200d
Saketh Rajesh <saketh@Sakeths-Laptop.local>
2023-08-11 23:00:28 -0400

    initial

---
+++ landing_site/app/(site)/page.tsx
@@ -0,0 +1,40 @@
+import About from "@/components/About";
+import Blog from "@/components/Blog";
+import CTA from "@/components/CTA";
+import Contact from "@/components/Contact";
+import FAQ from "@/components/FAQ";
+import Feature from "@/components/Features";
+import FeaturesTab from "@/components/FeaturesTab";
+import FunFact from "@/components/FunFact";
+import Hero from "@/components/Hero";
+import Integration from "@/components/Integration";
+import Pricing from "@/components/Pricing";
+import Testimonial from "@/components/Testimonial";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  description: "This is Home for Solid Pro",
+  // other metadata
+};
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      {/* <Brands /> */}
+      <Feature />
+      <About />
+      <FeaturesTab />
+      <FunFact />
+      <Integration />
+      <CTA />
+      <FAQ />
+      <Testimonial />
+      <Pricing />
+      <Contact />
+      <Blog />
+    </main>
+  );
+}
